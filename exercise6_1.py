73a9468ad279122d9ff6faf02c9de60bfc2dbc35
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-10 20:45:48 -0400

    exercise 6_1 completed

--- exercise6_1.py
+++ exercise6_1.py
@@ -1,1 +1,5 @@
-def wrap_text()
+def wrap_text(word, symbols):
+    return "{}{}{}".format(symbols, word, symbols[::-1])
+
+print(wrap_text("hello", "==="))
+print(wrap_text("new message", "---===###"))
